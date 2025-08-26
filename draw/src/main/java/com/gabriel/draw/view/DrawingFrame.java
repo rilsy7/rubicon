45f5db187a2399ba20211c855fb1a7b6534771ad
rilsy7 <163081129+rilsy7@users.noreply.github.com>
2025-08-26 10:13:40 +0800

    Update DrawingFrame.java

--- draw/src/main/java/com/gabriel/draw/view/DrawingFrame.java
+++ draw/src/main/java/com/gabriel/draw/view/DrawingFrame.java
@@ -2,12 +2,13 @@ package com.gabriel.draw.view;
 
 import com.gabriel.draw.controller.DrawingWindowController;
 import com.gabriel.drawfx.model.Drawing;
+import com.gabriel.drawfx.ShapeMode;
 import com.gabriel.drawfx.service.AppService;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DrawingFrame extends JFrame {
-
 
     public DrawingFrame(AppService appService){
         DrawingWindowController drawingWindowController = new DrawingWindowController(appService);
@@ -15,7 +16,23 @@ public class DrawingFrame extends JFrame {
         this.addWindowFocusListener(drawingWindowController);
         this.addWindowStateListener(drawingWindowController);
 
+        JButton lineButton = new JButton("Line");
+        JButton rectButton = new JButton("Rectangle");
+        JButton ellipseButton = new JButton("Ellipse");
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(lineButton);
+        buttonPanel.add(rectButton);
+        buttonPanel.add(ellipseButton);
+
         DrawingView drawingView = new DrawingView(appService);
-        this.getContentPane().add(drawingView);
+
+        this.setLayout(new BorderLayout());
+        this.add(buttonPanel, BorderLayout.NORTH);
+        this.add(drawingView, BorderLayout.CENTER);
+
+        lineButton.addActionListener(e -> appService.setShapeMode(ShapeMode.Line));
+        rectButton.addActionListener(e -> appService.setShapeMode(ShapeMode.Rectangle));
+        ellipseButton.addActionListener(e -> appService.setShapeMode(ShapeMode.Ellipse));
     }
 }
